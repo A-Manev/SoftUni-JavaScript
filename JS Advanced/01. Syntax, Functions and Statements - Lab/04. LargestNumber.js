921b83b6e71845c23c62aa8504c5793fd481661b
A-Manev <sasho_123_456@abv.bg>
2021-01-07 12:41:25 +0200

    Solution to Largest Number

---
+++ JS Advanced/01. Syntax, Functions and Statements - Lab/04. LargestNumber.js
@@ -0,0 +1,8 @@
+function findLargestNumber(firstNumber, secondNumber, thirdNumber){
+    let largestNumber = Math.max(firstNumber, secondNumber, thirdNumber);
+
+    console.log(`The largest number is ${largestNumber}.`)
+}
+
+findLargestNumber(5, -3, 16);
+findLargestNumber(-3, -5, -22.5);
