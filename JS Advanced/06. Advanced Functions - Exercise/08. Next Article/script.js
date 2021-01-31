e80cb4cd3939f1cbdaad10403c65c873cf1207cc
A-Manev <sasho_123_456@abv.bg>
2021-01-31 21:31:49 +0200

    Solution for problem Next Article

---
+++ JS Advanced/06. Advanced Functions - Exercise/08. Next Article/script.js
@@ -0,0 +1,11 @@
+function getArticleGenerator(articles) {
+    let content = document.getElementById('content');
+
+    return function showNext() {
+        if (articles.length) {
+            let article = document.createElement('article');
+            article.textContent = articles.shift();
+            content.appendChild(article);
+        }
+    }
+}
