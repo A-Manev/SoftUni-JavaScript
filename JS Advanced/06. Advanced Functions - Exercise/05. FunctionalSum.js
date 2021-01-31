f1e335e76d5f1589de4c22f4b89cae57c56cda50
A-Manev <sasho_123_456@abv.bg>
2021-01-31 21:30:25 +0200

    Solution for problem Functional Sum

---
+++ JS Advanced/06. Advanced Functions - Exercise/05. FunctionalSum.js
@@ -0,0 +1,15 @@
+function add(a) {
+    let sum = 0;
+    sum += a;
+
+    function calc(b) {
+        sum += b;
+        return calc;
+    }
+
+    calc.toString = () => sum;
+    return calc;
+}
+
+console.log(add(1));
+console.log(add(1)(6)(-3));
