2061b30c8d52ae185871e4b46ed63e8c66d5fe9e
A-Manev <sasho_123_456@abv.bg>
2021-02-11 20:52:23 +0200

    Solution for problem Person

---
+++ JS Advanced/09. Prototypes and Inheritance - Lab/01. Person.js
@@ -0,0 +1,36 @@
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    set fullName(value) {
+        let [firstName, lastName] = value.split(' ');
+
+        if (firstName && lastName) {
+            this.lastName = lastName;
+            this.firstName = firstName;
+        }
+    }
+}
+
+let person = new Person("Peter", "Ivanov");
+console.log(person.fullName); //Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName); //George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName); //George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName); //Nikola
+console.log(person.lastName); //Tesla
+let person = new Person("Albert", "Simpson");
+console.log(person.fullName); //Albert Simpson
+person.firstName = "Simon";
+console.log(person.fullName); //Simon Simpson
+person.fullName = "Peter";
+console.log(person.firstName);  // Simon
+console.log(person.lastName);  // Simpson
