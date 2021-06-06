b7538fcdd027082b084c31917aa3df25b04e05c3
A-Manev <sasho_123_456@abv.bg>
2021-06-06 17:26:31 +0300

    Converted all storage methods to database

--- JS Back-end/MongoDB and Mongoose - Workshop/Cubicle/services/storage.js
+++ JS Back-end/MongoDB and Mongoose - Workshop/Cubicle/services/storage.js
@@ -1,15 +1,4 @@
 const Cube = require('../models/Cube');
-const fs = require('fs/promises');
-const uniqid = require('uniqid');
-
-// load and parse data file
-// provide ability to:
-// - read all entries
-// - read single entry by ID
-// - add new entry
-// * get matching entries by search criteria
-
-let data = {};
 
 /* Model structure */
 /*
@@ -22,12 +11,6 @@ let data = {};
 */
 
 async function init() {
-    try {
-        data = JSON.parse(await fs.readFile('./models/data.json'));
-    } catch (err) {
-        console.error('Error reading database');
-    }
-
     return (req, res, next) => {
         req.storage = {
             getAll,
@@ -40,28 +23,29 @@ async function init() {
 }
 
 async function getAll(query) {
-    let cubes = Object
-        .entries(data)
-        .map(([id, v]) => Object.assign({}, { id }, v));
+    const options = {};
 
     // filter cubes by query params
     if (query.search) {
-        cubes = cubes.filter(c => c.name.toLowerCase().includes(query.search.toLowerCase()));
+        options.name = { $regex: query.search, $options: 'i' };
     }
     if (query.from) {
-        cubes = cubes.filter(c => c.difficulty >= Number(query.from));
+        options.difficulty = { $gte: Number(query.from) };
     }
     if (query.to) {
-        cubes = cubes.filter(c => c.difficulty <= Number(query.to));
+        options.difficulty = options.difficulty || {};
+        options.difficulty.$lte = Number(query.to);
     }
+
+    const cubes = Cube.find(options).lean();
 
     return cubes;
 }
 
 async function getById(id) {
-    const cube = data[id];
+    const cube = await Cube.findById(id).lean();
     if (cube) {
-        return Object.assign({}, { id }, cube);
+        return cube;
     } else {
         return undefined;
     }
@@ -73,21 +57,13 @@ async function create(cube) {
 }
 
 async function edit(id, cube) {
-    if (!data[id]) {
+    const existing = await Cube.findById(id);
+    if (!existing) {
         throw new ReferenceError('No such ID in database');
     }
-    data[id] = cube;
 
-    await persist();
-}
-
-async function persist() {
-    try {
-        await fs.writeFile('./models/data.json', JSON.stringify(data, null, 2));
-        console.log('>>> created new record');
-    } catch (err) {
-        console.error('Error writing out database');
-    }
+    Object.assign(existing, cube);
+    return existing.save();
 }
 
 module.exports = {
